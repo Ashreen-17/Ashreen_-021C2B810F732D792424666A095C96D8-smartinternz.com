f5a034926e4ede8ef1f91f6efc4fa9722a337750
Ashreen-17 <144922534+Ashreen-17@users.noreply.github.com>
2023-09-27 12:03:29 +0530

    Add files via upload

--- main (2).py
+++ main (2).py
@@ -1,30 +1,8 @@
-class Bank_Account:
-    def __init__(self):
-        self.balance=0
-        print("Hello!!! Welcome to the Deposit & Withdrawal Machine")
- 
-    def deposit(self):
-        amount=float(input("Enter amount to be Deposited: "))
-        self.balance += amount
-        print("\n Amount Deposited:",amount)
- 
-    def withdraw(self):
-        amount = float(input("Enter amount to be Withdrawn: "))
-        if self.balance>=amount:
-            self.balance-=amount
-            print("\n You Withdrew:", amount)
-        else:
-            print("\n Insufficient balance  ")
- 
-    def display(self):
-        print("\n Net Available Balance=",self.balance)
- 
-# Driver code
-  
-# creating an object of class
-s = Bank_Account()
-  
-# Calling functions with that class object
-s.deposit()
-s.withdraw()
-s.display()
+n = 23
+fact = 1
+
+for i in range(1, n + 1):
+  fact = fact * i
+
+print("The factorial of 23 is : ", end="")
+print(fact)
